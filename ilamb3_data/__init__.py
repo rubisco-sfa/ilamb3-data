6a23e04c85b628461a023588dee2238d312c35dd
Nathan Collier <nathaniel.collier@gmail.com>
2025-02-24 12:38:08 -0500

    added assets for deps

--- ilamb3_data/__init__.py
+++ ilamb3_data/__init__.py
@@ -1,4 +1,8 @@
+import os
+
 import pooch
+import requests
+from tqdm import tqdm
 
 
 def create_registry(registry_file: str) -> pooch.Pooch:
@@ -19,3 +23,26 @@ def create_registry(registry_file: str) -> pooch.Pooch:
     )
     registry.load_registry(registry_file)
     return registry
+
+
+def download_file(remote_source: str, local_source: str | None = None) -> str:
+    """
+    Download the specified file to a local location.
+    """
+    if local_source is None:
+        local_source = os.path.basename(remote_source)
+    if not os.path.isfile(local_source):
+        resp = requests.get(remote_source, stream=True)
+        total_size = int(resp.headers.get("content-length"))
+        with open(local_source, "wb") as fdl:
+            with tqdm(
+                total=total_size,
+                unit="B",
+                unit_scale=True,
+                desc=local_source,
+            ) as pbar:
+                for chunk in resp.iter_content(chunk_size=1024):
+                    if chunk:
+                        fdl.write(chunk)
+                        pbar.update(len(chunk))
+    return local_source

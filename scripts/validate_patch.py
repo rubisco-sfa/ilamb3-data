620832695f0a53ea905b0035bb7925b1e483aa51
Nathan Collier <nathaniel.collier@gmail.com>
2025-02-24 13:09:13 -0500

    update a check for if something in the data directory was changed

--- scripts/validate_patch.py
+++ scripts/validate_patch.py
@@ -8,13 +8,17 @@ from ilamb3_data import create_registry
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
-        print("No registry files have changed.")
+        print("No registry entries have changed.")
 
     # Parse out the lines in the registry that have been added
     content = " ".join(sys.argv[1:])
     match = re.search(r"registry/data.txt:\s\[([\d,\s]*)\]", content)
     if not match:
-        print("No registry files have changed.")
+        if "data/" in content:
+            raise ValueError(
+                "It appears you are adding a new dataset, but we do not see a change in the registry. Did you add the output of `sha1sum FILE.nc` to `registry/data.txt`?"
+            )
+        print("No registry entries have changed.")
         sys.exit(0)
     lines = [int(i.strip()) - 1 for i in match.group(1).split(",")]
 

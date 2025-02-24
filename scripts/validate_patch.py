95d4750b64d741ae16e3667ca998d5738de205e2
Nathan Collier <nathaniel.collier@gmail.com>
2025-02-24 08:31:12 -0500

    try reading output file

--- scripts/validate_patch.py
+++ scripts/validate_patch.py
@@ -1,4 +1,9 @@
+import json
 import sys
 
 if __name__ == "__main__":
     print(sys.argv)
+    with open(sys.argv[1]) as fin:
+        lines_changed = json.load(fin)
+        print(type(lines_changed))
+        print(lines_changed)

4c5e6604586d2410b357b31c87fc241aa50b8c20
msteckle <morganrsteckler@gmail.com>
2025-03-20 14:15:39 -0400

    added lat/lon attribute checks to validator

--- scripts/validate_dataset.py
+++ scripts/validate_dataset.py
@@ -64,16 +64,16 @@ class ILAMBDataset(BaseModel):
     @field_validator("ds")
     @classmethod
     def check_vars(cls, ds: xr.Dataset) -> xr.Dataset:
-        # Check that the dataset has at least one variable but not more than 2
+        # Check that there are data variables
         if not ds.data_vars:
             raise ValueError(
                 "Dataset does not have any data variables. An example data variable is 'cSoil'."
             )
+        # Check that the dataset has at least one variable but not more than 2
         if len(ds.data_vars) >= 3:
             raise ValueError(
                 f"Dataset has too many data variables {ds.data_vars}. The measurement and the uncertainty are the only expected data variables. There should be one netCDF file per data variable if a dataset has multiple data variables."
             )
-        return ds
 
     @field_validator("ds")
     @classmethod
@@ -104,8 +104,13 @@ class ILAMBDataset(BaseModel):
         time_dim_present = "time" in dimensions
         time_var = ds["time"]
         time_attrs = time_var.attrs
+
+        # Check if the time dimension is present
         if not time_dim_present:
-            raise ValueError(f"Dataset does not have a time dimension, {dimensions=}")
+            raise ValueError(
+                f"Dataset does not have a time dimension, {dimensions=}. Expected a dimension called 'time'."
+            )
+
         # Check if time values are decoded as datetime objects
         time_dtype = type(time_var.values[0])
         if not (
@@ -116,21 +121,33 @@ class ILAMBDataset(BaseModel):
                 f"Time values are not properly decoded as datetime objects: {time_dtype=}"
             )
 
-        # Check for time attributes: axis, long_name
+        # Check time attributes: axis, long_name, standard_name
         missing = set(["axis", "long_name", "standard_name"]) - set(time_attrs)
         if missing:
             raise ValueError(
-                f"Dataset is missing time-specific attributes, {missing=}. We recommend axis=T"
+                f"Dataset is missing time-specific attributes, {missing=}."
             )
+        else:
+            # Check the axis and standard_name to ensure they're correct; long_name can vary.
+            correct_axis_name = time_attrs["axis"] == "T"
+            if not correct_axis_name:
+                raise TypeError(
+                    f"The time dimension's axis attribute is {time_attrs['axis']}. Expected 'T'."
+                )
+            correct_std_name = time_attrs["standard_name"] == "time"
+            if not correct_std_name:
+                raise TypeError(
+                    f"The time dimension's standard_name attribute is {time_attrs['standard_name']}. Expected 'time'."
+                )
 
-        # Check that time units are encoded (and formatted correctly)
+        # Check time units encoding and formatting
         time_encoding = time_var.encoding
         if "units" not in time_encoding or "since" not in time_encoding["units"]:
             raise ValueError(
                 f"Time encoding is missing or incorrect, {time_encoding=}. Expected 'days since YYYY:MM:DD HH:MM'"
             )
 
-        # Check if time calendar is encoded
+        # Check time calendar encoding
         if "calendar" in time_encoding:
             valid_calendars = [
                 "standard",
@@ -171,14 +188,14 @@ class ILAMBDataset(BaseModel):
         else:
             raise ValueError("Calendar attribute is missing from the time encoding.")
 
-        # Check if bounds are encoded
+        # Check bounds encoding
         time_bounds_name = time_attrs["bounds"]
         if time_bounds_name not in ds:
             raise ValueError(
                 f"Time bounds variable '{time_bounds_name=}' is missing from dataset. Expected 'time_bounds'"
             )
 
-        # Validate time_bounds structure
+        # Check time_bounds structure
         time_bounds = ds[time_bounds_name]
         if len(time_bounds.dims) != 2 or time_bounds.dims[0] != "time":
             raise ValueError(
@@ -193,7 +210,7 @@ class ILAMBDataset(BaseModel):
                 "The second dimension should have length 2 to represent time bounds."
             )
 
-        # Check for the correct 'long_name' attribute
+        # Check for the correct 'long_name' attribute for time_bounds
         if (
             "long_name" not in time_bounds.attrs
             or time_bounds.attrs["long_name"] != "time_bounds"
@@ -213,29 +230,52 @@ class ILAMBDataset(BaseModel):
         dims_lower = {
             dim.lower(): dim for dim in dims
         }  # Map lowercased dims to original names
-        lat_name = next(
-            (
-                dims_lower[name.lower()]
-                for name in lat_names
-                if name.lower() in dims_lower
-            ),
-            None,
-        )
+        lat_names_found = [
+            dims_lower[name.lower()] for name in lat_names if name.lower() in dims_lower
+        ]
 
-        # check that one of the accepted latitutde long_names exists
-        if not lat_name:
+        # Ensure there is only one latitude dimension
+        if len(lat_names_found) != 1:
             raise ValueError(
-                f"Dataset does not have an accepted latitude dimension, {dims=}. Expected one of {lat_names} (case insensitive)."
+                f"Dataset has {len(lat_names_found)} latitude dimensions, expected exactly one. Found: {lat_names_found}"
             )
 
-        # check that axis and long_name attributes are present
+        lat_name = lat_names_found[0]
+
+        # Check that one of the accepted latitude long_names exists
+        lat_var = ds[lat_name]
+        lat_attrs = lat_var.attrs
+
+        # Check for missing latitude attributes
+        missing = set(["axis", "long_name", "standard_name", "units"]) - set(lat_attrs)
+        if missing:
+            raise ValueError(
+                f"Dataset is missing latitude-specific attributes, {missing=}"
+            )
         else:
-            lat_var = ds[lat_name]
-            lat_attrs = lat_var.attrs
-            missing = set(["axis", "long_name"]) - set(lat_attrs)
-            if missing:
+            # Check axis
+            if lat_attrs["axis"] != "Y":
                 raise ValueError(
-                    f"Dataset is missing latitude-specific attributes, {missing=}"
+                    f"Incorrect latitude axis attribute: {lat_attrs['axis']}. Expected 'Y' (case sensitive)."
+                )
+            # Check standard_name
+            if lat_attrs["standard_name"] != "latitude":
+                raise ValueError(
+                    f"Incorrect latitude standard_name attribute: {lat_attrs['standard_name']}. Expected 'latitude' (case sensitive)."
+                )
+            # Check units
+            valid_lat_units = {
+                "degrees_north",
+                "degree_north",
+                "degree_N",
+                "degrees_N",
+                "degreeN",
+                "degreesN",
+            }
+            lat_units = lat_attrs.get("units")
+            if lat_units not in valid_lat_units:
+                raise ValueError(
+                    f"Invalid 'units' attribute for latitude dimension. Found: {lat_units}. Expected one of {valid_lat_units}."
                 )
 
         return ds
@@ -243,35 +283,59 @@ class ILAMBDataset(BaseModel):
     @field_validator("ds")
     @classmethod
     def lon_dim(cls, ds: xr.Dataset) -> xr.Dataset:
-        # Check that the dataset has a properly set-up latitude dimension
+        # Check that the dataset has a properly set-up longitude dimension
         lon_names = {"lon", "longitude", "x"}
         dims = ds.dims
         dims_lower = {
             dim.lower(): dim for dim in dims
         }  # Map lowercased dims to original names
-        lon_name = next(
-            (
-                dims_lower[name.lower()]
-                for name in lon_names
-                if name.lower() in dims_lower
-            ),
-            None,
-        )
+        lon_names_found = [
+            dims_lower[name.lower()] for name in lon_names if name.lower() in dims_lower
+        ]
 
-        # check that one of the accepted latitutde long_names exists
-        if not lon_name:
+        # Ensure there is only one longitude dimension
+        if len(lon_names_found) != 1:
             raise ValueError(
-                f"Dataset does not have an accepted latitude dimension, {dims=}. Expected one of {lon_names} (case insensitive)."
+                f"Dataset has {len(lon_names_found)} longitude dimensions, expected exactly one. Found: {lon_names_found}"
             )
 
-        # check that axis and long_name attributes are present
+        lon_name = lon_names_found[0]
+
+        # Check that one of the accepted longitude long_names exists
+        lon_var = ds[lon_name]
+        lon_attrs = lon_var.attrs
+
+        # Check for missing longitude attributes
+        missing = set(["axis", "long_name", "standard_name", "units"]) - set(lon_attrs)
+        if missing:
+            raise ValueError(
+                f"Dataset is missing longitude-specific attributes, {missing=}"
+            )
         else:
-            lon_var = ds[lon_name]
-            lon_attrs = lon_var.attrs
-            missing = set(["axis", "long_name"]) - set(lon_attrs)
-            if missing:
+            # Check axis
+            if lon_attrs["axis"] != "X":
                 raise ValueError(
-                    f"Dataset is missing latitude-specific attributes, {missing=}"
+                    f"Incorrect latitude axis attribute: {lon_attrs['axis']}. Expected 'X' (case sensitive)"
+                )
+            # Check standard_name
+            if lon_attrs["standard_name"] != "longitude":
+                raise ValueError(
+                    f"Incorrect latitude standard_name attribute: {lon_attrs['standard_name']}. Expected 'longitude' (case sensitive)"
+                )
+
+            # Check units
+            valid_lon_units = {
+                "degrees_east",
+                "degree_east",
+                "degree_E",
+                "degrees_E",
+                "degreeE",
+                "degreesE",
+            }
+            lon_units = lon_attrs.get("units")
+            if lon_units not in valid_lon_units:
+                raise ValueError(
+                    f"Invalid 'units' attribute for longitude dimension. Found: {lon_units}. Expected one of {valid_lon_units}."
                 )
 
         return ds

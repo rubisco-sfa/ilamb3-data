4f7866af22c3475b55ffac3c3068530bfba77774
msteckle <morganrsteckler@gmail.com>
2026-01-28 15:44:35 -0500

    bugfix: supposedly resolved issue with weird global mean and timeseries

--- data/GFED-5-0/convert.py
+++ data/GFED-5-0/convert.py
@@ -1,12 +1,15 @@
 import glob
 import os
+import warnings
 import zipfile
 from datetime import datetime
 from pathlib import Path
 
+import matplotlib.pyplot as plt
 import numpy as np
 import requests
 import xarray as xr
+from matplotlib.colors import LogNorm
 
 from ilamb3_data import (
     create_output_filename,
@@ -21,6 +24,12 @@ from ilamb3_data import (
     set_time_attrs,
     set_var_attrs,
     standardize_dim_order,
+)
+
+warnings.filterwarnings(
+    "ignore",
+    message="invalid value encountered in divide",
+    category=RuntimeWarning,
 )
 
 # Set download parameters
@@ -47,17 +56,20 @@ for record in records:
 record = data["hits"]["hits"][0]
 
 # Download
+print(f"\nSelected dataset: {record['metadata'].get('title')}")
 download_from_zenodo(record, download_dir="_raw")
 
-# Unzip downloaded data file (BA.zip)
-path_to_zip_file = Path("_raw/BA.zip")
-full_path = os.path.abspath(path_to_zip_file)
-full_path_without_zip, _ = os.path.splitext(full_path)
-with zipfile.ZipFile(path_to_zip_file, "r") as zip_ref:
-    zip_ref.extractall(full_path_without_zip)
+# Unzip downloaded ZIP files
+raw_dir = Path("_raw")
+for zip_path in raw_dir.glob("*.zip"):
+    full_path = zip_path.resolve()
+    out_dir = full_path.with_suffix("")  # strip .zip
+    with zipfile.ZipFile(full_path, "r") as zip_ref:
+        zip_ref.extractall(out_dir)
 
 # Set timestamps and tracking id
-download_stamp = gen_utc_timestamp(path_to_zip_file.stat().st_mtime)
+latest_zip = max(raw_dir.glob("*.zip"), key=lambda p: p.stat().st_mtime)
+download_stamp = gen_utc_timestamp(latest_zip.stat().st_mtime)
 creation_stamp = gen_utc_timestamp()
 today_stamp = datetime.now().strftime("%Y%m%d")
 tracking_id = gen_trackingid()
@@ -67,33 +79,34 @@ tracking_id = gen_trackingid()
 ######################################################################
 
 # Get a list of all netCDF files in the unzipped folder
-data_dir = "_raw/BA"
-all_files = glob.glob(os.path.join(data_dir, "*.nc"))
+nc_dir = raw_dir / "BA"
+all_files = glob.glob(os.path.join(nc_dir, "*.nc"))
 
 # Get separate lists for coarse data and fine data
-coarse_files = []  # For 1997-2000 (1 degree)
-fine_files = []  # For 2001-2020 (0.25 degree)
+files_97_00 = []  # For 1997-2000 (1 degree)
+files_01_20 = []  # For 2001-2020 (0.25 degree)
 for f in all_files:
     basename = os.path.basename(f)  # e.g., "BA200012.nc"
     # Extract the year from the filename; characters at positions 2:6.
     year = int(basename[2:6])
     if year < 2001:
-        coarse_files.append(f)
+        files_97_00.append(f)
     else:
-        fine_files.append(f)
+        files_01_20.append(f)
+files_97_00 = sorted(files_97_00)
+files_01_20 = sorted(files_01_20)
 
 # Load the coarse and fine datasets separately
 time_coder = xr.coders.CFDatetimeCoder(use_cftime=True)
-ds_coarse = xr.open_mfdataset(
-    coarse_files, combine="by_coords", decode_times=time_coder
-)
-ds_fine = xr.open_mfdataset(fine_files, combine="by_coords", decode_times=time_coder)
+ds_97_00 = xr.open_mfdataset(files_97_00, decode_times=time_coder)
+ds_01_20 = xr.open_mfdataset(files_01_20, decode_times=time_coder)
 
 # Load burnable area (and mask) datasets
-da_coarse_mask = xr.open_dataset(
-    "_raw/BurnableArea_preMOD.nc", decode_times=time_coder
-)["BurableArea"]  # note the mispelling of Burnable in the original file
-da_fine_mask = xr.open_dataset("_raw/BurnableArea.nc", decode_times=time_coder)[
+# Sum of 0.25deg burnable area == sum of 1deg burnable area
+ba_97_00 = xr.open_dataset("_raw/BurnableArea_preMOD.nc", decode_times=time_coder)[
+    "BurableArea"
+]  # note the mispelling of Burnable...
+ba_01_20 = xr.open_dataset("_raw/BurnableArea.nc", decode_times=time_coder)[
     "BurableArea"
 ]
 
@@ -101,30 +114,38 @@ da_fine_mask = xr.open_dataset("_raw/BurnableArea.nc", decode_times=time_coder)[
 # Process netcdfs
 ######################################################################
 
-# Calculate burned fraction of burnable area as a percent
-percent_burned_coarse = (ds_coarse["Total"] / da_coarse_mask) * 100
-ds_coarse = ds_coarse.assign({"burntFractionAll": percent_burned_coarse})
-percent_burned_fine = (ds_fine["Total"] / da_fine_mask) * 100
-ds_fine = ds_fine.assign({"burntFractionAll": percent_burned_fine})
+# Coarse data (1997-2000)
+# -----------------------
 
-# Mask the datasets
-percent_burned_coarse_masked = ds_coarse.where(da_coarse_mask > 0)
-percent_burned_fine_masked = ds_fine.where(da_fine_mask > 0)
+# Set target grid for interpolation (0.25 degree)
+newlat = ba_01_20["lat"]
+newlon = ba_01_20["lon"]
 
-# Interpolate coarse 1 degree data to 0.25 degrees
-res = 0.25
-newlon = np.arange(-180, 180, res)
-newlat = np.arange(-90, 90, res)
-percent_burned_coarse_masked_interp = percent_burned_coarse_masked.interp(
-    method="nearest", lat=newlat, lon=newlon
-)
+# Map coarse burned area and coarse burnable area onto .25deg grid
+# Nearest is fine because the 1deg and 0.25deg grids are aligned (i.e.,
+# 0.25deg grid points fall exactly on 1deg grid points)
+ds_97_00 = ds_97_00["Total"].interp(lat=newlat, lon=newlon, method="nearest")
+ba_97_00 = ba_97_00.interp(lat=newlat, lon=newlon, method="nearest")
+
+# Downscale burned area (km² → km² on 0.25°) & convert to percent of burnable area
+# 1deg data at 0.25deg cells * burnable area fraction of 1deg cell
+tot_ds_97_00 = ds_97_00 * xr.where(ba_97_00 > 0, ba_01_20 / ba_97_00, 0.0)
+pct_ds_97_00 = xr.where(ba_01_20 > 0, 100.0 * tot_ds_97_00 / ba_01_20, np.nan)
+ds_97_00 = pct_ds_97_00.to_dataset(name="burntFractionAll")
+
+# Fine data (2001-2020)
+# ---------------------
+ds_01_20 = ds_01_20.where(ba_01_20 > 0)
+pct_ds_01_20 = (ds_01_20["Total"] / ba_01_20) * 100
+ds_01_20 = ds_01_20.assign({"burntFractionAll": pct_ds_01_20})
+
+# For some reason, one single gridcell of bFA is > 100%; so mask it
+ds_01_20 = ds_01_20.where(ds_01_20["burntFractionAll"] <= 100)
 
 # Combine coarse-interpolated and fine data into one dataset at 0.25 degree resolution
-ds = xr.concat(
-    [percent_burned_fine_masked, percent_burned_coarse_masked_interp], dim="time"
-)
-ds = ds.sortby("time")
-ds = ds["burntFractionAll"].to_dataset()
+da = xr.concat([ds_01_20["burntFractionAll"], ds_97_00["burntFractionAll"]], dim="time")
+da = da.sortby("time")
+ds = da.to_dataset()
 
 ######################################################################
 # Set CF compliant netcdf attributes
@@ -146,7 +167,7 @@ ds = set_var_attrs(
     cmip6_units=var_info["variable_units"],
     cmip6_standard_name=var_info["cf_standard_name"],
     cmip6_long_name=var_info["variable_long_name"],
-    target_dtype=np.int8,
+    target_dtype=np.float32,
     convert=False,
 )
 
@@ -192,5 +213,39 @@ out_ds = set_ods26_global_attrs(
 )
 
 # Prep for export
-out_path = create_output_filename(ds.attrs)
-ds.to_netcdf(out_path, format="NETCDF4")
+out_path = create_output_filename(out_ds.attrs)
+out_ds.to_netcdf(out_path, format="NETCDF4")
+
+######################################################################
+# Plotting verification
+######################################################################
+
+# Spatial mean
+mean_da = out_ds["burntFractionAll"].mean(dim="time") * 12  # annual mean
+mean_da = mean_da.where(mean_da > 0)
+plt.figure(figsize=(10, 5))
+plt.pcolormesh(
+    mean_da["lon"],
+    mean_da["lat"],
+    mean_da,
+    norm=LogNorm(vmin=0.1, vmax=100),
+    cmap="jet",
+)
+cbar = plt.colorbar()
+cbar.set_label("Burned fraction (%)")
+plt.tight_layout()
+plt.savefig(
+    "/Users/6ru/Desktop/ilamb3-data/data/GFED-5-0/mean_burntFractionAll.png", dpi=300
+)
+
+
+# Temporal mean
+mean_time_series = out_ds["burntFractionAll"].mean(dim=["lat", "lon"])
+plt.figure()
+mean_time_series.plot()
+plt.ylim(0, 1)
+plt.ylabel("Global Mean Burnt Fraction (%)")
+plt.title("GFED-5 Burnt Fraction Time Series (1997-2020)")
+plt.savefig(
+    "/Users/6ru/Desktop/ilamb3-data/data/GFED-5-0/burntFractionAll_timeseries.png"
+)

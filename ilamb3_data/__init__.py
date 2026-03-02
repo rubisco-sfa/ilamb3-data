0eff84a94331ace6c25e9b35fe759cf27b1f28a0
msteckle <morganrsteckler@gmail.com>
2026-03-02 13:02:13 -0500

    FIX: added missing GDAL dependency needed to run HWSD2 convert script; removed dead set_ods func from init; updated get_cmip6_variable_info to raise error when nothing comes up

--- ilamb3_data/__init__.py
+++ ilamb3_data/__init__.py
@@ -5,7 +5,6 @@ import re
 import urllib.request
 import uuid
 import warnings
-from calendar import monthrange
 from typing import Optional
 
 import cftime as cf
@@ -156,10 +155,12 @@ def get_cmip6_variable_info(
     if variable_id:
         if variable_id not in df.index:
             raise ValueError(
-                f"Variable ID '{variable_id}' not found in CMIP6 variable info."
+                f"Variable ID '{variable_id}' not found in CMIP6 variable info. "
+                f"Use existing information from the {variable_id} attrs instead."
             )
         return df.loc[variable_id].to_dict()
-    return df.to_dict(orient="index")
+    else:
+        raise ValueError("variable_id must be provided to get specific variable info.")
 
 
 def time_bounds_from_frequency(
@@ -940,208 +941,6 @@ def get_nested_dict(data, path, default=None):
         except (IndexError, TypeError):
             return default
     return data
-
-
-def set_ods_global_attrs(
-    ds: xr.Dataset,
-    *,
-    activity_id="obs4MIPs",
-    aux_variable_id: Optional[str] = "N/A",
-    comment: Optional[str] = "N/A",
-    contact: str,
-    conventions="CF-1.12 ODS-2.5",
-    creation_date: str,
-    dataset_contributor: str,
-    data_specs_version: str,
-    doi: str = "N/A",
-    external_variables: Optional[str] = "N/A",
-    frequency: str,
-    grid: str,
-    grid_label: str,
-    has_auxdata: bool,
-    history: str,
-    institution: str,
-    institution_id: str,
-    license: str,
-    nominal_resolution: str,
-    processing_code_location: str,
-    product: str,
-    realm: str,
-    references: str,
-    region: str,
-    source: str,
-    source_id: str,
-    source_data_retrieval_date: Optional[str] = "N/A",
-    source_data_url: Optional[str] = "N/A",
-    source_label: str,
-    source_type: str,
-    source_version_number: str,
-    title: Optional[str] = "N/A",
-    tracking_id: str,
-    variable_id: str,
-    variant_label: str,
-    variant_info: str,
-    version: Optional[str] = str,
-) -> xr.Dataset:
-    """
-    Set required NetCDF global attributes according to CF-Conventions 1.12 and ODS-2.5.
-
-    This function validates that all required attributes are provided and assigns them
-    to the global attributes of the input xarray dataset. Optional fields may be set to None.
-
-    Special behavior:
-        - Attributes with default values must match those defaults unless explicitly overridden.
-        - Some attributes must be selected from a predefined list.
-        - Some attributes are validated against controlled vocabularies loaded from the official online JSON files.
-        - Nested vocabularies (e.g., obs4MIPs_CV.json) are accessed by key lookup (e.g., search "source_id").
-
-    Args:
-        ds (xr.Dataset): The xarray dataset to which global attributes will be added.
-
-    Returns:
-        xr.Dataset: The dataset with updated global attributes.
-
-    Raises:
-        ValueError: If any required attribute is missing or not valid.
-    """
-
-    valid_external_variables = ["areacella", "areacello", "volcella", "volcello"]
-
-    base_url = "https://raw.githubusercontent.com/PCMDI/obs4MIPs-cmor-tables/master/"
-
-    freq_cv = load_json_from_url(base_url + "obs4MIPs_frequency.json")
-    institution_cv = load_json_from_url(base_url + "obs4MIPs_institution_id.json")
-    nominal_res_cv = load_json_from_url(base_url + "obs4MIPs_nominal_resolution.json")
-    realm_cv = load_json_from_url(base_url + "obs4MIPs_realm.json")
-    region_cv = load_json_from_url(base_url + "obs4MIPs_region.json")
-    source_id_cv = load_json_from_url(base_url + "obs4MIPs_source_id.json")
-    source_type_cv = load_json_from_url(base_url + "obs4MIPs_source_type.json")
-    top_level_cv = load_json_from_url(base_url + "Tables/obs4MIPs_CV.json")
-    grid_labels_cv = load_json_from_url(base_url + "obs4MIPs_grid_label.json")
-    products_cv = load_json_from_url(base_url + "obs4MIPs_product.json")
-    if frequency == "mon":
-        mip_tables = [
-            "Tables/obs4MIPs_Lmon.json",
-            "Tables/obs4MIPs_Omon.json",
-            "Tables/obs4MIPs_Amon.json",
-        ]
-        for table in mip_tables:
-            if variable_id in get_nested_dict(
-                load_json_from_url(base_url + table), ["variable_entry"]
-            ):
-                realm = get_nested_dict(
-                    load_json_from_url(base_url + table), ["Header"]
-                )["realm"]
-                variable_cv = load_json_from_url(base_url + table)
-                table_name = table.split("_")[-1].split(".")[0]
-
-    errors = []
-    if has_auxdata:
-        if aux_variable_id == "None":
-            errors.append("must specify ancillary variable_ids if included")
-
-    # Check vals dependent on "valid_" lists hard-coded above
-    if grid_label not in grid_labels_cv["grid_label"]:
-        errors.append("grid_label must match a key in obs4MIPs_grid_label.json")
-    if product not in products_cv["product"]:
-        errors.append("product must match a key in obs4MIPs_product.json")
-    # if external_variables:
-    #     if external_variables not in valid_external_variables:
-    #         errors.append(
-    #             f"external_variables must be one of {valid_external_variables}"
-    #         )
-    # Check vals present in Github json files
-    if frequency not in freq_cv["frequency"]:
-        errors.append("frequency must match a key in obs4MIPs_frequency.json")
-    # if institution_id not in institution_cv["institution_id"]:
-    #     errors.append("institution_id must match a key in obs4MIPs_institution_id.json")
-    # if nominal_resolution not in nominal_res_cv["nominal_resolution"]:
-    #     errors.append(
-    #         "nominal_resolution must match a key in obs4MIPs_nominal_resolution.json"
-    #     )
-    if realm not in realm_cv["realm"]:
-        errors.append("realm must match a key in obs4MIPs_realm.json")
-    if region not in region_cv["region"]:
-        errors.append("region must match a key in obs4MIPs_region.json")
-    # if source_id not in source_id_cv["source_id"]:
-    #     errors.append("source_id must match a key in obs4MIPs_source_id.json")
-    # if source_type not in source_type_cv["source_type"]:
-    #     errors.append("source_type must match a key in obs4MIPs_source_type.json")
-    # Check vals *nested* within Github json files
-    # if source_label != get_nested_dict(
-    #     source_id_cv, ["source_id", source_id, "source_label"]
-    # ):
-    #     errors.append(
-    #         "source_label must match the label inside obs4MIPs_source_id.json[source_id]"
-    #     )
-    # if source_version_number not in get_nested_dict(
-    #     source_id_cv, ["source_id", source_id, "source_version_number"]
-    # ):
-    #     errors.append("source_version_number must match a value inside source_id entry")
-    # if source_id not in get_nested_dict(top_level_cv, ["CV", "source_id"]):
-    #     errors.append(
-    #         "source_id must be present in obs4MIPs_CV.json under 'source_id' key"
-    #     )
-    # if source not in get_nested_dict(
-    #     top_level_cv, ["CV", "source_id", source_id, "source"]
-    # ):
-    #     errors.append(
-    #         "source must match a attribute in the 'source_id' section of obs4MIPs_CV.json"
-    #     )
-    # if variable_id not in get_nested_dict(variable_cv, ["variable_entry"]):
-    #     errors.append(
-    #         f"variable_id must match a key in the 'variable_entry' section of {table_name}"
-    #     )
-
-    if errors:
-        raise ValueError("\n".join(errors))
-
-    attrs = {
-        "activity_id": activity_id,
-        "aux_variable_id": aux_variable_id,
-        "comment": comment,
-        "contact": contact,
-        "Conventions": conventions,
-        "creation_date": creation_date,
-        "dataset_contributor": dataset_contributor,
-        "data_specs_version": data_specs_version,
-        "doi": doi,
-        "frequency": frequency,  # https://github.com/PCMDI/obs4MIPs-cmor-tables/blob/master/obs4MIPs_frequency.json
-        "grid": grid,
-        "grid_label": grid_label,  # ["gn", "gr1"]
-        "has_auxdata": has_auxdata,
-        "history": history,
-        "institution": institution,
-        "institution_id": institution_id,  # have to be registered on https://github.com/PCMDI/obs4MIPs-cmor-tables/blob/master/obs4MIPs_institution_id.json
-        "license": license,
-        "nominal_resolution": nominal_resolution,  # https://github.com/PCMDI/obs4MIPs-cmor-tables/blob/master/obs4MIPs_nominal_resolution.json
-        "processing_code_location": processing_code_location,
-        "product": product,  # [“observations”, “reanalysis”, “in_situ”, “exploratory_product”]
-        "realm": realm,  # https://github.com/PCMDI/obs4MIPs-cmor-tables/blob/master/obs4MIPs_realm.json
-        "references": references,
-        "region": region,  # https://github.com/PCMDI/obs4MIPs-cmor-tables/blob/master/obs4MIPs_region.json
-        "source": source,  # https://github.com/PCMDI/obs4MIPs-cmor-tables/blob/master/Tables/obs4MIPs_CV.json (search source_id)
-        "source_data_retrieval_date": source_data_retrieval_date,
-        "source_data_url": source_data_url,
-        "source_id": source_id,  # https://github.com/PCMDI/obs4MIPs-cmor-tables/blob/master/obs4MIPs_source_id.json
-        "source_label": source_label,  # https://github.com/PCMDI/obs4MIPs-cmor-tables/blob/master/obs4MIPs_source_id.json (nested source_label)
-        "source_type": source_type,  # https://github.com/PCMDI/obs4MIPs-cmor-tables/blob/master/obs4MIPs_source_type.json
-        "source_version_number": source_version_number,  # https://github.com/PCMDI/obs4MIPs-cmor-tables/blob/master/obs4MIPs_source_id.json (nested source_version_number)
-        "title": title,
-        "tracking_id": tracking_id,  # automatically detected by CMOR (hdl:21.14102/<uuid>)
-        "variable_id": variable_id,  # https://clipc-services.ceda.ac.uk/dreq/index/CMORvar.html
-        "variant_info": variant_info,  # description of who prepared the data, describe obs data variance if applicable
-        "variant_label": variant_label,  # same as source_id (if prepped by them), or "BE" if source_id unknown, ensemble member identified via -r1, -r2, ..., -rN
-        "version": version,
-    }
-
-    missing = [k for k, v in attrs.items() if v is None]
-    if missing:
-        raise ValueError(f"Missing required global attributes: {', '.join(missing)}")
-
-    ds.attrs = attrs
-
-    return ds
 
 
 def set_ods26_global_attrs(
